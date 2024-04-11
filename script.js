4eafcc42e1f955a9d8522e90ca413b604a5fb811
LuuTanDat27 <115093574+LuuTanDat27@users.noreply.github.com>
2024-04-11 10:17:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ allLinks.forEach(link => {
         allTabs.forEach(tab => tab.classList.toggle("active-tab-content", tab.id === linkId));
     });
 });
+let lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop){
+        // Cuộn xuống dưới
+        document.querySelector('.scroll-header').classList.add('hidden');
+    } else {
+        // Cuộn lên đầu trang
+        document.querySelector('.scroll-header').classList.remove('hidden');
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
